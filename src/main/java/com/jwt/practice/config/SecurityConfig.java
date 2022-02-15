bfb7ed74a925b6c2fca8e1fe9f4797d05e6a75c8
bbeomgeun <miki308@naver.com>
2022-02-16 00:49:37 +0900

    create: CorsConfig.java and addFilter

--- src/main/java/com/jwt/practice/config/SecurityConfig.java
+++ src/main/java/com/jwt/practice/config/SecurityConfig.java
@@ -10,21 +10,26 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 X, stateless 서버로
                 .and()
+                .addFilter(corsFilter)  // 내 서버는 crossOrigin 요청이 와도 허용이 된다.
                 .formLogin().disable() // formLogin 사용 x
                 .httpBasic().disable() // 기본적인 http 로그인 방식 사용 x - jwt용 세팅
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/api/v1/admin/**")
+                .antMatchers("/api/v1/manager/**")
                 .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll();
     }
 }
