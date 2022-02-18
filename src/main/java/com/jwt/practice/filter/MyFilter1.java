5bbfa88c3c57d97aeb6a9e8c8c32231f612f2266
bbeomgeun <miki308@naver.com>
2022-02-18 12:12:54 +0900

    create: customFilter 1,2,3.class

---
+++ src/main/java/com/jwt/practice/filter/MyFilter1.java
@@ -0,0 +1,12 @@
+package com.jwt.practice.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter1 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("필터 1");
+        chain.doFilter(request, response); // 끝나지말고 계속 프로그램 프로세스가 진행되도록
+    }
+}
