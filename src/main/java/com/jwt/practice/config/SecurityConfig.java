d51acfa13094826cca4d5a97ebcec873571bdae6
bbeomgeun <miki308@naver.com>
2022-02-18 12:14:30 +0900

    update: addFilterBefore in SecurityConfig.class

--- src/main/java/com/jwt/practice/config/SecurityConfig.java
+++ src/main/java/com/jwt/practice/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.jwt.practice.config;
 
+import com.jwt.practice.filter.MyFilter1;
+import com.jwt.practice.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class); // 시큐리티 필터 이전에 걸어야 한다.
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 X, stateless 서버로
                 .and()
