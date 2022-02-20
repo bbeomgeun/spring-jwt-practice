4f494b408d155b471a14c78249979a642aad8ad2
bbeomgeun <miki308@naver.com>
2022-02-20 15:23:40 +0900

    update: token test filter 추가

--- src/main/java/com/jwt/practice/filter/MyFilter3.java
+++ src/main/java/com/jwt/practice/filter/MyFilter3.java
@@ -1,12 +1,27 @@
 package com.jwt.practice.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("필터 3");
-        chain.doFilter(request, response); // 끝나지말고 계속 프로그램 프로세스가 진행되도록
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (req.getMethod().equals("POST")) {
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println("headerAuth = " + headerAuth);
+
+            if (headerAuth.equals("test")) {
+                chain.doFilter(request, response); // 끝나지말고 계속 프로그램 프로세스가 진행되도록
+            } else {
+                PrintWriter writer = res.getWriter();
+                writer.println("인증 안됨");
+            }
+        }
     }
 }
