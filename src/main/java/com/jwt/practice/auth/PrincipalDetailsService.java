4a0ce70683fb94770d1977f33d694433dbe32ed1
bbeomgeun <miki308@naver.com>
2022-02-22 01:48:47 +0900

    create: PrincipalDetailsService.java

---
+++ src/main/java/com/jwt/practice/auth/PrincipalDetailsService.java
@@ -0,0 +1,22 @@
+package com.jwt.practice.auth;
+
+import com.jwt.practice.model.User;
+import com.jwt.practice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUserName(username);
+        return new PrincipalDetails(userEntity);
+    }
+}
